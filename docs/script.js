35baf4f6d97a8b7b64bdbbefab282cb59494d7f2
Robert Yang <chickennugget181@gmail.com>
2025-04-10 17:22:45 -0700

    Added draggable windows

---
+++ docs/script.js
@@ -0,0 +1,29 @@
+// Make all window elements (specifically their title bar) draggable:
+window.onload = function () {
+    var desktopWindows = document.getElementsByClassName("window");
+    Array.from(desktopWindows).forEach(element => {
+        console.log("we have an element!");
+        dragElement(element);
+    });
+}
+
+function dragElement(el) {
+    // elmnt is a list of all HTML elements by class name 'title-bar':
+    el.addEventListener('mousedown', function (e) {
+        var offsetX = e.clientX - parseInt(window.getComputedStyle(this).left);
+        var offsetY = e.clientY - parseInt(window.getComputedStyle(this).top);
+
+        function mouseMoveHandler(e) {
+            el.style.top = (e.clientY - offsetY) + 'px';
+            el.style.left = (e.clientX - offsetX) + 'px';
+        }
+
+        function reset() {
+            window.removeEventListener('mousemove', mouseMoveHandler);
+            window.removeEventListener('mouseup', reset);
+        }
+
+        window.addEventListener('mousemove', mouseMoveHandler);
+        window.addEventListener('mouseup', reset);
+    });
+}
