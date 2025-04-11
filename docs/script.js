36c7d80540357ffe476429a3dfc45db363b79394
Robert Yang <chickennugget181@gmail.com>
2025-04-10 18:05:16 -0700

    Changed so only title bar drags window around, can resize

--- docs/script.js
+++ docs/script.js
@@ -1,21 +1,23 @@
 // Make all window elements (specifically their title bar) draggable:
 window.onload = function () {
-    var desktopWindows = document.getElementsByClassName("window");
+    var desktopWindows = document.getElementsByClassName("title-bar");
     Array.from(desktopWindows).forEach(element => {
         dragElement(element);
     });
 }
 
 function dragElement(el) {
-    // elmnt is a list of all HTML elements by class name 'window':
-    // TODO: Change so we only click/drag `title-bar` and then entier window can move via parentElement
+    // elmnt is a list of all HTML elements by class name 'title-bar':
+    // We get parent Element for each "title-bar" and use that window to move
+    var parentElem = el.parentElement;
+    console.log(parentElem);
     el.addEventListener('mousedown', function (e) {
-        var offsetX = e.clientX - parseInt(window.getComputedStyle(this).left);
-        var offsetY = e.clientY - parseInt(window.getComputedStyle(this).top);
+        var offsetX = e.clientX - parseInt(window.getComputedStyle(parentElem).left);
+        var offsetY = e.clientY - parseInt(window.getComputedStyle(parentElem).top);
 
         function mouseMoveHandler(e) {
-            el.style.top = (e.clientY - offsetY) + 'px';
-            el.style.left = (e.clientX - offsetX) + 'px';
+            parentElem.style.top = (e.clientY - offsetY) + 'px';
+            parentElem.style.left = (e.clientX - offsetX) + 'px';
         }
 
         function reset() {
